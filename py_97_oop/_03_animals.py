967625c24c962d3ec8ed06d0efe4443e070c1a15
kimikanna <svartirose@gmail.com>
2024-05-19 16:37:21 +0500

    class Animals is now abstract

--- py_97_oop/_03_animals.py
+++ py_97_oop/_03_animals.py
@@ -2,24 +2,41 @@
 # и реализуйте метод voice. Реализуйте класс Dog. Придумайте, какие переменные будет принимать данный класс
 # и какие методы будут реализованы. Реализуйте в этом классе паттерн Singleton
 
-class Animals:
+from abc import ABCMeta, abstractmethod
 
-    def __init__(self, age: int, color: str, paws_count: int = 4) -> None:
+
+class Animals:
+    """Abstract class for Animals"""
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self, age: int, color: str, voice_tone: str, paws_count: int = 4) -> None:
         self.age = age
         self.color = color
+        self.voice_tone = voice_tone
         self.paws_count = paws_count
+
+    @abstractmethod
+    def voice(self):
+        """Узнать голос"""
 
 
 class Cat(Animals):
-    @staticmethod
-    def voice() -> str:
-        return "Meow"
+    """Class for Cats"""
+
+    def __init__(self, age: int, color: str, voice_tone: str, paws_count: int = 4) -> None:
+        super().__init__(age, color, voice_tone, paws_count)
+
+    def voice(self) -> str:
+        return f"Cat's voice is: {self.voice_tone}"
 
     def info(self) -> str:
         return f"Cat's age is: {self.age}, color: {self.color}"
 
 
 class Dog(Animals):
+    """Class for Dogs, using singleton"""
+
     __instance = None
 
     def __new__(cls, *args, **kwargs):
@@ -28,22 +45,27 @@ class Dog(Animals):
 
         return cls.__instance
 
-    def __init__(self, age, color, name, paws_count=4) -> None:
-        super().__init__(age, color, paws_count)
+    def __init__(self, age: int, color: str, voice_tone: str, name: str, paws_count: int = 4) -> None:
+        super().__init__(age, color, voice_tone, paws_count)
         self.name = name
+
+    def voice(self) -> str:
+        return f"Dog's voice is: {self.voice_tone}"
 
     def info(self) -> str:
         return f"Dog's name is: {self.name}, age: {self.age}, color: {self.color}"
 
 
-my_cat = Cat(3, "Gray")
+my_cat = Cat(3, "Gray", "Meow")
 print(f"Cat's voice is: {my_cat.voice()}")
 print(my_cat.info())
 
 print("-----------")
 
-dog_1 = Dog(5, "Black", "Bim")
+dog_1 = Dog(5, "Black", "Bark", "Bim")
 print(dog_1.info())
-dog_2 = Dog(3, "Ginger", "Dark")
+print(dog_1.voice())
+dog_2 = Dog(3, "Ginger", "Howl", "Dark")
 print(dog_2.info())
 print(dog_1.info())  # Outputs dog_2 data, because of singleton
+print(dog_1.voice())  # Outputs "Howl", because of singleton
